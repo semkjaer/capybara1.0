6d56e7b9637627ec09cedfc9c7ea0d5e919fa7e5
semkjaer <semkjaer@hotmail.nl>
2023-06-13 12:08:22 +0200

    data 2013-2022 (incomplete), design update

--- application/src/pages/1 Jeugdzorg in kaart.py
+++ application/src/pages/1 Jeugdzorg in kaart.py
@@ -14,11 +14,43 @@ from utils import auth, logo, get_data
 
 logo()
 
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"JPEG"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('application/src/background.JPEG') 
+
 if 'key' not in st.session_state:
         st.session_state['key'] = str(uuid.uuid4())
 
+st.markdown('''
+<style>
+[data-testid="stHeader"] {
+        display: none;
+}
+[data-baseweb="base-input"] {
+        border:1px solid black; 
+}
+.streamlit-expander { 
+        background-color: #FFFFFF; 
+}
+</style>
+''', unsafe_allow_html=True)
+
 authenticator, config = auth()
-name, authentication_status, username = authenticator.login('Login', 'main')
+with st.expander('Login', expanded=True):
+    name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status:
     authenticator.logout('Logout', 'sidebar')
@@ -32,25 +64,26 @@ fig, ax = plt.subplots(figsize = (12,12))
 ax.set_axis_off()
 
 if st.session_state["authentication_status"]:
-    st.title("Jeugdzorg in kaart")
-    st.write("This is the model page.")
-    with open("gemeentes.txt") as file:
-        for line in file:
-            options = line.split(",")
-    option = st.selectbox("Selecteer gebied:", np.append(["Nederland"], gdf.GM_NAAM.unique()[1:]))
-    if option == "Nederland":
-        gdf.plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
-        complete = True
-    else:
-        try:
-            gemeentecode = gdf[gdf.GM_NAAM == option].gemeentecode.unique()[1]
-            gdf[gdf.gemeentecode == gemeentecode].plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"}, edgecolor="black")
+    with st.expander('Home page', expanded=True):
+        st.title("Jeugdzorg in kaart")
+        st.write("This is the model page.")
+        with open("gemeentes.txt") as file:
+            for line in file:
+                options = line.split(",")
+        option = st.selectbox("Selecteer gebied:", np.append(["Nederland"], gdf.GM_NAAM.unique()[1:]))
+        if option == "Nederland":
+            gdf.plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
             complete = True
-        except:
-            st.write("Helaas is er voor deze gemeente geen data beschikbaar")
-            complete = False
-    if complete:
-        st.pyplot(fig)
+        else:
+            try:
+                gemeentecode = gdf[gdf.GM_NAAM == option].gemeentecode.unique()[1]
+                gdf[gdf.gemeentecode == gemeentecode].plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"}, edgecolor="black")
+                complete = True
+            except:
+                st.write("Helaas is er voor deze gemeente geen data beschikbaar")
+                complete = False
+        if complete:
+            st.pyplot(fig)
 elif authentication_status == False:
      st.error('Username/password is incorrect')
 
