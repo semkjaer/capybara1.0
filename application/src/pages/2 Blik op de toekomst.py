4e9b887be0eb314548727014036b3cc044c722c0
semkjaer <semkjaer@hotmail.nl>
2023-06-13 12:08:22 +0200

    data 2013-2022 (incomplete), design update

--- application/src/pages/2 Blik op de toekomst.py
+++ application/src/pages/2 Blik op de toekomst.py
@@ -10,8 +10,46 @@ from utils import auth, get_data, logo, model
 
 logo()
 
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"JPEG"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('application/src/background.JPEG') 
+
 if 'key' not in st.session_state:
         st.session_state['key'] = str(uuid.uuid4())
+
+st.markdown('''
+<style>
+.st-dq { background-color: #FFFFFF; }
+.streamlit-expander { background-color: #FFFFFF; }
+.css-dbxtwr { display: none; }
+</style>
+''', unsafe_allow_html=True)
+st.markdown('''
+<style>
+[data-testid="stHeader"] {
+        display: none;
+}
+[data-baseweb="base-input"] {
+        border:1px solid black; 
+}
+.streamlit-expander { 
+        background-color: #FFFFFF; 
+}
+</style>
+''', unsafe_allow_html=True)
 
 authenticator, config = auth()
 name, authentication_status, username = authenticator.login('Login', 'main')
