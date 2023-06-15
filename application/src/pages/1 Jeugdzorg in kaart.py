6a02d5fbbf0014ec3b11c41bdb14ef2863eb1454
Stan Fris <stanfris2.0@gmail.com>
2023-06-15 16:08:13 +0200

    folium map

--- application/src/pages/1 Jeugdzorg in kaart.py
+++ application/src/pages/1 Jeugdzorg in kaart.py
@@ -9,6 +9,8 @@ import os
 import uuid
 import streamlit as st
 import streamlit_authenticator as stauth
+from streamlit_folium import st_folium
+import folium
 
 from utils import auth, logo, get_data
 
@@ -66,10 +68,7 @@ ax.set_axis_off()
 if st.session_state["authentication_status"]:
     with st.expander('Home page', expanded=True):
         st.title("Jeugdzorg in kaart")
-        st.write("This is the model page.")
-        with open("gemeentes.txt") as file:
-            for line in file:
-                options = line.split(",")
+        st.write("Hier kan je kaarten bekijken")
         option = st.selectbox("Selecteer gebied:", np.append(["Nederland"], gdf.GM_NAAM.unique()[1:]))
         if option == "Nederland":
             gdf.plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
@@ -77,7 +76,19 @@ if st.session_state["authentication_status"]:
         else:
             try:
                 gemeentecode = gdf[gdf.GM_NAAM == option].gemeentecode.unique()[1]
-                gdf[gdf.gemeentecode == gemeentecode].plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"}, edgecolor="black")
+                df = gdf[gdf.gemeentecode == gemeentecode]
+                df = df.to_crs(epsg=4326)
+                m = folium.Map(location=[52.1326, 5.2913], zoom_start=9, tiles="CartoDB positron")
+                for _, r in df.iterrows():
+                    # Without simplifying the representation of each borough,
+                    # the map might not be displayed
+                    sim_geo = gpd.GeoSeries(r["geometry"]).simplify(tolerance=0.003)
+                    geo_j = sim_geo.to_json()
+                    geo_j = folium.GeoJson(data=geo_j, style_function=lambda x: {"fillColor": "orange"})
+                    folium.Popup(r["WK_NAAM"]).add_to(geo_j)
+                    geo_j.add_to(m)
+                output = st_folium(m)
+                st.write(output)
                 complete = True
             except:
                 st.write("Helaas is er voor deze gemeente geen data beschikbaar")
@@ -86,44 +97,3 @@ if st.session_state["authentication_status"]:
             st.pyplot(fig)
 elif authentication_status == False:
      st.error('Username/password is incorrect')
-
-#  data imports
-import pandas as pd
-import numpy as np
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import os
-
-# app imports
-import uuid
-import streamlit as st
-import streamlit_authenticator as stauth
-
-from utils import auth, logo, get_data
-
-logo()
-
-if 'key' not in st.session_state:
-        st.session_state['key'] = str(uuid.uuid4())
-
-authenticator, config = auth()
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-if authentication_status:
-    authenticator.logout('Logout', 'sidebar')
-
-# if os.path.exists('./data_combined.csv'):
-#          df = gpd.read_csv('./data_combined.csv')
-
-# df = get_data()
-# gdf = gpd.GeoDataFrame(df, crs="EPSG:28992", geometry=df.geometry)
-# fig, ax = plt.subplots(figsize = (12,12))
-# gdf.plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
-
-if st.session_state["authentication_status"]:
-    st.title("Jeugdzorg in kaart")
-    st.write("This is the model page.")
-    # st.pyplot(fig)
-elif authentication_status == False:
-     st.error('Username/password is incorrect')
-
