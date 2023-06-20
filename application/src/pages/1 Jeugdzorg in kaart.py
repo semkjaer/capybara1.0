807e9c56ba4e7e7e846ccf6a968c23090cc7dd76
Stan Fris <stanfris2.0@gmail.com>
2023-06-20 16:13:50 +0200

    working geomap with hover, click, custom legend and neighbourhood selection

--- application/src/pages/1 Jeugdzorg in kaart.py
+++ application/src/pages/1 Jeugdzorg in kaart.py
@@ -81,15 +81,27 @@ if st.session_state["authentication_status"]:
             gdf.plot(ax=ax, column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
             complete = True
         else:
-            try:
-                gemeentecode = gdf[gdf.GM_NAAM == option].gemeentecode.unique()[1]
-                plt = gdf[gdf.gemeentecode == gemeentecode].explore(column="AANT_INW",legend=True, legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"})
-                output = st_folium(plt)
-                st.write(output)
+            # try:
+                wijk = st.selectbox("Selecteer wijk (optioneel):", np.append(["Gehele gemeente"], gdf[gdf.GM_NAAM == option].WK_NAAM.unique()))
+                gemeentecode = gdf[gdf.GM_NAAM == option].gemeentecode.unique()[0]
+                if wijk != "Gehele gemeente":
+                    wijkcode = gdf[gdf.WK_NAAM == wijk].WK_CODE.unique()[0]
+                    plt = gdf[gdf.WK_CODE == wijkcode].explore(column="perc_jhzv",tooltip=["WK_NAAM", "woonwaarde", "perc_jhzv"], popup=["WK_NAAM", "woonwaarde", "perc_jhzv"], legend="False", legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"}, vmin=4, vmax=15)
+                    output = st_folium(plt, returned_objects=[])
+                    st.write(output)
+
+                else:
+                    plt = gdf[gdf.gemeentecode == gemeentecode].explore(column="perc_jhzv", tooltip=["WK_NAAM", "woonwaarde", "perc_jhzv"], popup=["WK_NAAM", "woonwaarde", "perc_jhzv"], legend="False", legend_kwds={"label": "Aantal mensen per gemeente", "orientation": "horizontal"}, vmin=4, vmax=15)
+                    output = st_folium(plt, returned_objects=[])
+                    # this prevents a goofy error and the returning of single brackets with nothing inside because the st_folium library doesnt work well
+                    try:
+                         st.write(output[0])
+                    except:
+                         pass
                 complete = True
-            except:
-                st.write("Helaas is er voor deze gemeente geen data beschikbaar")
-                complete = False
+            # except:
+            #     st.write("Helaas is er voor deze gemeente of wijken geen data beschikbaar")
+            #     complete = False
         if complete:
             st.pyplot(fig)
 elif authentication_status == False:
