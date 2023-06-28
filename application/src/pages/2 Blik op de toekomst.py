d5bd3a088061e1428c83b01793e3c1dab78c0e3d
Stan Fris <stanfris2.0@gmail.com>
2023-06-28 14:49:36 +0200

    Blik op de toekomst

--- application/src/pages/2 Blik op de toekomst.py
+++ application/src/pages/2 Blik op de toekomst.py
@@ -49,7 +49,7 @@ st.markdown('''
 }
 [data-baseweb="select"] {
     margin-top: -40px;
-    margin-bottom: -20px;
+    border:1px solid grey;
 }
 .block-container {
     padding-top: 0px !important;
@@ -81,7 +81,7 @@ if authentication_status:
 
 if st.session_state["authentication_status"]:
     with st.expander('', expanded=True):
-        st.title("Blik op de toekomst")
+        st.text("")
         ts_total = pd.read_csv("ts_total.csv")
         shades_of_pink = ['#FC6C85', '#FC8EAC', '#F88379', '#FF9999', '#FFD1DC', '#FFB6C1', '#FFB7C5', 
                     '#FFC1CC', '#F4C2C2', '#E75480', '#FF007F', '#A94064', '#FF6EC7', '#DE5D83', 
@@ -126,7 +126,7 @@ if st.session_state["authentication_status"]:
                                     name=wijk,
                                     legendgroup=wijk,
                                     line={'dash': 'solid', 'color': color},
-                                    mode='lines')
+                                    mode='lines', )
                 
                 ts_wijk2 = ts_plot2[(ts_plot2['regio'] == wijk)]
                 trace2 = go.Scatter(x=ts_wijk2['year'],
@@ -159,70 +159,76 @@ if st.session_state["authentication_status"]:
                         layer="below",
                         line_width=0,
                         )
-                ]
+                ],
+                title="Data en voorspelling jeugdhulp zonder verblijf"
         )
         with col1:
                 st.plotly_chart(fig, use_container_width=True)
 
-        ts_bar_23_sorted = ts_plot.loc[(ts_plot['year'] == '2023-01-01')].sort_values(by = 'p_jz_tn', ascending=False)
+        gemeentedata = pd.read_csv("gemeentedata.csv")
+        gemeentedata = gemeentedata[gemeentedata["Gemeentenaam_18"].str.strip() == option]
 
-        fig = px.bar(x=ts_bar_23_sorted['p_jz_tn'][:20].round(2), 
-                y=ts_bar_23_sorted.regio[ts_bar_23_sorted['p_jz_tn'].index][:20],
-                title='Wijken met het hoogst voorspelde percentage jongeren met jeugdzorg in 2023', 
-                template='plotly_white')
+        # Melt the dataframe to transform the columns into rows
+        gemeentedata = gemeentedata[["ID","k_0Tot4Jaar_2", "k_4Tot12Jaar_3", "k_12Tot18Jaar_4", "k_18Tot23Jaar_5"]]
+        gemeentedata.columns = ["ID","kinderen tot 4", "kinderen tussen 4 en 12", "kinderen tussen 12 en 18", "kinderen tussen 18 en 24"]
+        gemeentedata = gemeentedata.melt(id_vars='ID', var_name='AgeGroup', value_name='Aantal')
 
-        fig.update_layout(yaxis = {"categoryorder":"total ascending"},
-                        xaxis_title="Percentage jongeren met jeugdzorg",
-                        yaxis_title="Wijk")
-
-        fig.update_traces(marker_color='#E2007A')
+        # Create the pie chart using Plotly Express
+        fig = px.pie(gemeentedata, values='Aantal', names='AgeGroup', title="Verdeling leeftijdsgroepen in gemeente voor jeugdhulp")
+        fig.update_layout(colorway=shades_of_pink)
+        
         with col2:
                 st.plotly_chart(fig, use_container_width=True)
+
 
         ts_pie = ts_total[ts_total.gm_naam == option]
         ts_23_sorted = ts_pie.loc[(ts_pie['year'] == '2023-01-01')]
         ts_22_sorted = ts_pie.loc[(ts_pie['year'] == '2022-01-01')]
-        
-        fig = px.pie(names=ts_23_sorted.regio[ts_23_sorted['p_jz_tn'].index][:20].sort_values(ascending=True), 
-                values=ts_23_sorted['p_jz_tn'][:20].round(2).sort_values(ascending=True), 
-                title='Wijken en hun voorspelde percentage jongeren met jeugdzorg in 2023')
-        fig.update_layout(colorway=shades_of_pink)
+        if ts_plot["regio"].unique().size > 20:
+                ts_bar_23_sorted = ts_plot.loc[(ts_plot['year'] == '2023-01-01')].sort_values(by = 'p_jz_tn', ascending=False)
+
+                fig = px.bar(x=ts_bar_23_sorted['p_jz_tn'][:10].round(2), 
+                        y=ts_bar_23_sorted.regio[ts_bar_23_sorted['p_jz_tn'].index][:10],
+                        title='Wijken met het hoogst voorspelde percentage jongeren met jeugdzorg in 2023', 
+                        template='plotly_white')
+
+                fig.update_layout(yaxis = {"categoryorder":"total ascending"},
+                                xaxis_title="Percentage jongeren met jeugdzorg",
+                                yaxis_title="Wijk")
+
+                fig.update_traces(marker_color='#E2007A')
+        else:
+                fig = px.pie(names=ts_23_sorted.regio[ts_23_sorted['p_jz_tn'].index][:20].sort_values(ascending=True), 
+                        values=ts_23_sorted['p_jz_tn'][:20].round(2).sort_values(ascending=True), 
+                        title='Wijken en hun voorspelde percentage jongeren met jeugdzorg in 2023')
+                fig.update_layout(colorway=shades_of_pink)
         with col1:
                 st.plotly_chart(fig, use_container_width=True)
 
-        fig = px.bar(x=ts_22_sorted.regio[ts_22_sorted['a_soz_ao'].index][:20].sort_values(ascending=True), 
-                y=ts_22_sorted['a_soz_ao'][:20].round(2).sort_values(ascending=True), 
-                title='Aantal WAO-gerechtigden in 2022', 
+        with col2:
+                ts_22_sorted.columns = ["", "gwb_code_10", "gm_naam", "regio", "Aantal mensen met AO uitkering", "Aantal mensen met OW uitkering", "Aantal mensen met WB uitkering", "Aantal mensen met WW uitkering", "Bevolkingsdichtheid", "Gemiddeld inkomen per inwoner", "Gemiddeld inkomen per inkomensontvanger", "20% huishoudens met hoogste inkomen", " 40% huishoudens met laagste inkomen", "Huishoudens met een laag inkomen", "Huish. onder of rond sociaal minimum", "Huurwoningen totaal", "20% personen met hoogste inkomen", "40% personen met laagste inkomen", "p_jz_tn", "Koopwoningen", "year"]
+                ts_total_alt = ts_total
+                ts_total_alt.columns = ["", "gwb_code_10", "gm_naam", "regio", "Aantal mensen met AO uitkering", "Aantal mensen met OW uitkering", "Aantal mensen met WB uitkering", "Aantal mensen met WW uitkering", "Bevolkingsdichtheid", "Gemiddeld inkomen per inwoner", "Gemiddeld inkomen per inkomensontvanger", "20% huishoudens met hoogste inkomen", " 40% huishoudens met laagste inkomen", "Huishoudens met een laag inkomen", "Huish. onder of rond sociaal minimum", "Huurwoningen totaal", "20% personen met hoogste inkomen", "40% personen met laagste inkomen", "p_jz_tn", "Koopwoningen", "year"]
+                ts_22_plot = ts_22_sorted.drop(["gwb_code_10", "gm_naam", "regio", "p_jz_tn", "Koopwoningen", "year"], axis=1)
+                to_plot = st.selectbox("", ts_22_plot.columns[1:])
+                fig = px.bar(x=ts_22_sorted.regio[ts_22_sorted[to_plot].index][:20], 
+                y=ts_22_sorted[to_plot][:20].round(2), 
+                title='{}'.format(to_plot), 
                 template='plotly_white')
 
-        fig.add_bar(x=['Gemiddelde in Nederland'], 
-                    y=[ts_total.loc[(ts_total['year'] == '2022-01-01')]['a_soz_ao'].mean()], 
-                    name='Nederlands gemiddelde', 
-                    showlegend=False)
+                fig.add_bar(x=['Gemiddelde in Nederland'], 
+                        y=[ts_total_alt.loc[(ts_total_alt['year'] == '2022-01-01')][to_plot].mean()], 
+                        name='Nederlands gemiddelde', 
+                        showlegend=False)
+                fig.update_layout(xaxis_title="Wijken",
+                                yaxis_title="{}".format(to_plot))
+                fig.update_traces(marker_color='#E2007A')
 
-        fig.update_layout(xaxis_title="Wijken",
-                        yaxis_title="Aantal WAO-gerechtigden")
-        fig.update_traces(marker_color='#E2007A')
-        with col2:
                 st.plotly_chart(fig, use_container_width=True)
 
-        fig = px.bar(x=ts_22_sorted.regio[ts_22_sorted['bev_dich'].index][:20].sort_values(ascending=True), 
-                y=ts_22_sorted['bev_dich'][:20].round(2).sort_values(ascending=True), 
-                title='Bevolkingsdichtheid in 2022', 
-                template='plotly_white')
 
-        fig.add_bar(x=['Gemiddelde in Nederland'], 
-                    y=[ts_total.loc[(ts_total['year'] == '2022-01-01')]['bev_dich'].mean()], 
-                    name='Nederlands gemiddelde', 
-                    showlegend=False)
 
-        fig.update_layout(xaxis_title="Wijken",
-                        yaxis_title="Bevolkingsdichtheid")
-        fig.update_traces(marker_color='#E2007A')
-        with col1:
-                st.plotly_chart(fig, use_container_width=True)
-        with col2:
-                st.image('importances.png')
+
 
 
 elif authentication_status == False:
